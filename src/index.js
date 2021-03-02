bf7fe4e563b6b5bddad286142e18d99da33085b4
AlexeyEmshanov <alexey.emshanov@gmail.com>
2021-03-02 22:46:01 +0400

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  let result = []  
+  if (array === undefined || array.length === 0){
+    result.push(0)
+  } else {
+    result = array.sort((elemA, elemB) => { return elemA - elemB})  
+  }
+  return result[0]
 }
 
 exports.max = function max (array) {
-  return 0;
+  let result = []  
+  if (array === undefined || array.length === 0){
+    result.push(0)
+  } else {
+    result = array.sort((elemA, elemB) => { return -(elemA - elemB)})  
+  }
+  return result[0]
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let result = 0  
+  if (array === undefined || array.length === 0){
+    return 0
+  } else {
+    result = array.reduce((acc, elem) => {return acc += elem}, 0)  
+  }
+  return (result / array.length)
 }
